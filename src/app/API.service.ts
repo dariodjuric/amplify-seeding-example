46407495a63d28af6d2dedd42fd6af6b27e27e81
Dario Djuric <dario.djuric@gmail.com>
2021-08-20 15:21:05 +0200

    feat: add restaurant api

---
+++ src/app/API.service.ts
@@ -0,0 +1,512 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+import { Injectable } from "@angular/core";
+import API, { graphqlOperation, GraphQLResult } from "@aws-amplify/api-graphql";
+import { Observable } from "zen-observable-ts";
+
+export interface SubscriptionResponse<T> {
+  value: GraphQLResult<T>;
+}
+
+export type CreateRestaurantInput = {
+  id?: string | null;
+  name: string;
+  description: string;
+  city: string;
+  _version?: number | null;
+};
+
+export type ModelRestaurantConditionInput = {
+  name?: ModelStringInput | null;
+  description?: ModelStringInput | null;
+  city?: ModelStringInput | null;
+  and?: Array<ModelRestaurantConditionInput | null> | null;
+  or?: Array<ModelRestaurantConditionInput | null> | null;
+  not?: ModelRestaurantConditionInput | null;
+};
+
+export type ModelStringInput = {
+  ne?: string | null;
+  eq?: string | null;
+  le?: string | null;
+  lt?: string | null;
+  ge?: string | null;
+  gt?: string | null;
+  contains?: string | null;
+  notContains?: string | null;
+  between?: Array<string | null> | null;
+  beginsWith?: string | null;
+  attributeExists?: boolean | null;
+  attributeType?: ModelAttributeTypes | null;
+  size?: ModelSizeInput | null;
+};
+
+export enum ModelAttributeTypes {
+  binary = "binary",
+  binarySet = "binarySet",
+  bool = "bool",
+  list = "list",
+  map = "map",
+  number = "number",
+  numberSet = "numberSet",
+  string = "string",
+  stringSet = "stringSet",
+  _null = "_null"
+}
+
+export type ModelSizeInput = {
+  ne?: number | null;
+  eq?: number | null;
+  le?: number | null;
+  lt?: number | null;
+  ge?: number | null;
+  gt?: number | null;
+  between?: Array<number | null> | null;
+};
+
+export type Restaurant = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type UpdateRestaurantInput = {
+  id: string;
+  name?: string | null;
+  description?: string | null;
+  city?: string | null;
+  _version?: number | null;
+};
+
+export type DeleteRestaurantInput = {
+  id: string;
+  _version?: number | null;
+};
+
+export type ModelRestaurantFilterInput = {
+  id?: ModelIDInput | null;
+  name?: ModelStringInput | null;
+  description?: ModelStringInput | null;
+  city?: ModelStringInput | null;
+  and?: Array<ModelRestaurantFilterInput | null> | null;
+  or?: Array<ModelRestaurantFilterInput | null> | null;
+  not?: ModelRestaurantFilterInput | null;
+};
+
+export type ModelIDInput = {
+  ne?: string | null;
+  eq?: string | null;
+  le?: string | null;
+  lt?: string | null;
+  ge?: string | null;
+  gt?: string | null;
+  contains?: string | null;
+  notContains?: string | null;
+  between?: Array<string | null> | null;
+  beginsWith?: string | null;
+  attributeExists?: boolean | null;
+  attributeType?: ModelAttributeTypes | null;
+  size?: ModelSizeInput | null;
+};
+
+export type ModelRestaurantConnection = {
+  __typename: "ModelRestaurantConnection";
+  items?: Array<Restaurant | null> | null;
+  nextToken?: string | null;
+  startedAt?: number | null;
+};
+
+export type CreateRestaurantMutation = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type UpdateRestaurantMutation = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type DeleteRestaurantMutation = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type SyncRestaurantsQuery = {
+  __typename: "ModelRestaurantConnection";
+  items?: Array<{
+    __typename: "Restaurant";
+    id: string;
+    name: string;
+    description: string;
+    city: string;
+    _version: number;
+    _deleted?: boolean | null;
+    _lastChangedAt: number;
+    createdAt: string;
+    updatedAt: string;
+  } | null> | null;
+  nextToken?: string | null;
+  startedAt?: number | null;
+};
+
+export type GetRestaurantQuery = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ListRestaurantsQuery = {
+  __typename: "ModelRestaurantConnection";
+  items?: Array<{
+    __typename: "Restaurant";
+    id: string;
+    name: string;
+    description: string;
+    city: string;
+    _version: number;
+    _deleted?: boolean | null;
+    _lastChangedAt: number;
+    createdAt: string;
+    updatedAt: string;
+  } | null> | null;
+  nextToken?: string | null;
+  startedAt?: number | null;
+};
+
+export type OnCreateRestaurantSubscription = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type OnUpdateRestaurantSubscription = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type OnDeleteRestaurantSubscription = {
+  __typename: "Restaurant";
+  id: string;
+  name: string;
+  description: string;
+  city: string;
+  _version: number;
+  _deleted?: boolean | null;
+  _lastChangedAt: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class APIService {
+  async CreateRestaurant(
+    input: CreateRestaurantInput,
+    condition?: ModelRestaurantConditionInput
+  ): Promise<CreateRestaurantMutation> {
+    const statement = `mutation CreateRestaurant($input: CreateRestaurantInput!, $condition: ModelRestaurantConditionInput) {
+        createRestaurant(input: $input, condition: $condition) {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {
+      input
+    };
+    if (condition) {
+      gqlAPIServiceArguments.condition = condition;
+    }
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <CreateRestaurantMutation>response.data.createRestaurant;
+  }
+  async UpdateRestaurant(
+    input: UpdateRestaurantInput,
+    condition?: ModelRestaurantConditionInput
+  ): Promise<UpdateRestaurantMutation> {
+    const statement = `mutation UpdateRestaurant($input: UpdateRestaurantInput!, $condition: ModelRestaurantConditionInput) {
+        updateRestaurant(input: $input, condition: $condition) {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {
+      input
+    };
+    if (condition) {
+      gqlAPIServiceArguments.condition = condition;
+    }
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <UpdateRestaurantMutation>response.data.updateRestaurant;
+  }
+  async DeleteRestaurant(
+    input: DeleteRestaurantInput,
+    condition?: ModelRestaurantConditionInput
+  ): Promise<DeleteRestaurantMutation> {
+    const statement = `mutation DeleteRestaurant($input: DeleteRestaurantInput!, $condition: ModelRestaurantConditionInput) {
+        deleteRestaurant(input: $input, condition: $condition) {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {
+      input
+    };
+    if (condition) {
+      gqlAPIServiceArguments.condition = condition;
+    }
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <DeleteRestaurantMutation>response.data.deleteRestaurant;
+  }
+  async SyncRestaurants(
+    filter?: ModelRestaurantFilterInput,
+    limit?: number,
+    nextToken?: string,
+    lastSync?: number
+  ): Promise<SyncRestaurantsQuery> {
+    const statement = `query SyncRestaurants($filter: ModelRestaurantFilterInput, $limit: Int, $nextToken: String, $lastSync: AWSTimestamp) {
+        syncRestaurants(filter: $filter, limit: $limit, nextToken: $nextToken, lastSync: $lastSync) {
+          __typename
+          items {
+            __typename
+            id
+            name
+            description
+            city
+            _version
+            _deleted
+            _lastChangedAt
+            createdAt
+            updatedAt
+          }
+          nextToken
+          startedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {};
+    if (filter) {
+      gqlAPIServiceArguments.filter = filter;
+    }
+    if (limit) {
+      gqlAPIServiceArguments.limit = limit;
+    }
+    if (nextToken) {
+      gqlAPIServiceArguments.nextToken = nextToken;
+    }
+    if (lastSync) {
+      gqlAPIServiceArguments.lastSync = lastSync;
+    }
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <SyncRestaurantsQuery>response.data.syncRestaurants;
+  }
+  async GetRestaurant(id: string): Promise<GetRestaurantQuery> {
+    const statement = `query GetRestaurant($id: ID!) {
+        getRestaurant(id: $id) {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {
+      id
+    };
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <GetRestaurantQuery>response.data.getRestaurant;
+  }
+  async ListRestaurants(
+    filter?: ModelRestaurantFilterInput,
+    limit?: number,
+    nextToken?: string
+  ): Promise<ListRestaurantsQuery> {
+    const statement = `query ListRestaurants($filter: ModelRestaurantFilterInput, $limit: Int, $nextToken: String) {
+        listRestaurants(filter: $filter, limit: $limit, nextToken: $nextToken) {
+          __typename
+          items {
+            __typename
+            id
+            name
+            description
+            city
+            _version
+            _deleted
+            _lastChangedAt
+            createdAt
+            updatedAt
+          }
+          nextToken
+          startedAt
+        }
+      }`;
+    const gqlAPIServiceArguments: any = {};
+    if (filter) {
+      gqlAPIServiceArguments.filter = filter;
+    }
+    if (limit) {
+      gqlAPIServiceArguments.limit = limit;
+    }
+    if (nextToken) {
+      gqlAPIServiceArguments.nextToken = nextToken;
+    }
+    const response = (await API.graphql(
+      graphqlOperation(statement, gqlAPIServiceArguments)
+    )) as any;
+    return <ListRestaurantsQuery>response.data.listRestaurants;
+  }
+  OnCreateRestaurantListener: Observable<
+    SubscriptionResponse<OnCreateRestaurantSubscription>
+  > = API.graphql(
+    graphqlOperation(
+      `subscription OnCreateRestaurant {
+        onCreateRestaurant {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`
+    )
+  ) as Observable<SubscriptionResponse<OnCreateRestaurantSubscription>>;
+
+  OnUpdateRestaurantListener: Observable<
+    SubscriptionResponse<OnUpdateRestaurantSubscription>
+  > = API.graphql(
+    graphqlOperation(
+      `subscription OnUpdateRestaurant {
+        onUpdateRestaurant {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`
+    )
+  ) as Observable<SubscriptionResponse<OnUpdateRestaurantSubscription>>;
+
+  OnDeleteRestaurantListener: Observable<
+    SubscriptionResponse<OnDeleteRestaurantSubscription>
+  > = API.graphql(
+    graphqlOperation(
+      `subscription OnDeleteRestaurant {
+        onDeleteRestaurant {
+          __typename
+          id
+          name
+          description
+          city
+          _version
+          _deleted
+          _lastChangedAt
+          createdAt
+          updatedAt
+        }
+      }`
+    )
+  ) as Observable<SubscriptionResponse<OnDeleteRestaurantSubscription>>;
+}
---
+++ src/models/index.d.ts
@@ -0,0 +1,20 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+type RestaurantMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class Restaurant {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly city: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Restaurant, RestaurantMetaData>);
+  static copyOf(source: Restaurant, mutator: (draft: MutableModel<Restaurant, RestaurantMetaData>) => MutableModel<Restaurant, RestaurantMetaData> | void): Restaurant;
+}
